b35bc826d41c999ebf468eb2173a86bb3d663073
Ronny R. Polloqueri <ronny.polloqueri@gmail.com>
2014-03-16 14:00:39 -0500

    nuevo codigo de hola

--- application/controllers/hola.php
+++ application/controllers/hola.php
@@ -13,22 +13,28 @@ class Hola extends CI_Controller
 	{
 		echo "saludo2";
 	}
-	function saludo1($id=0)
+	function saludo1($id=0, $nombre="horacio", $edad=23)
 	{
 		switch ($id) {
 			case 0:
-				echo "saludo 0";
+				echo "saludo 0".$nombre.$edad;
 				break;
 			case 1:
 				echo "saludo1";
 				break;
 			case 2:
 				echo "saludo2";
+
 			default:
 				# code...
 				echo "no es";
 				break;
+
 		}
+	}
+	function argumentos($nombre="horacio", $edad=23)
+	{
+		echo "hola",$nombre,"tienes esta edad",$edad;
 	}
 }
 
